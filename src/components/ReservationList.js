eda0663b6a3958fdeb4cc8c740e92a4ec732a9f9
Marwane OUKACHA <oukachamarwane0@gmail.com>
2025-04-08 17:35:04 +0000

    commit

--- src/components/ReservationList.js
+++ src/components/ReservationList.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import {
-  FaUserAlt,
+  FaTicketAlt,
   FaCalendarAlt,
   FaTrash,
   FaCheckCircle,
@@ -92,7 +92,7 @@ export default function ReservationList() {
               >
                 <div className="flex-1 space-y-4 sm:space-y-0 sm:flex sm:items-center sm:gap-10 text-lg">
                   <div className="flex items-center gap-3 text-gray-800">
-                    <FaUserAlt className="text-accent w-5 h-5" />
+                    <FaTicketAlt className="text-accent w-5 h-5" />
                     <span className="font-semibold">{res.guestName}</span>
                   </div>
 
@@ -136,8 +136,7 @@ export default function ReservationList() {
                   <p><strong>Special Requests:</strong> Early check-in, sea view</p>
                   <div className="flex gap-4 mt-4">
                     <button className="text-red-500 hover:text-red-700 transition">
-                    <FaTimes size={18} onClick={() => handleDeleteClick(res.id)} />
-
+                      <FaTimes size={18} onClick={() => handleDeleteClick(res.id)} />
                     </button>
                     <FaPen size={18} className="text-green-700 hover:text-accent transition"/>
                     <FaDownload size={18} className="text-gray hover:text-accent transition"/>
